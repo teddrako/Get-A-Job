a5bd81c37ed28b2877488dbd7af8b0ced79b54df
Ted <97199415+teddrako@users.noreply.github.com>
2023-09-14 12:11:38 -0400

    Summary

---
+++ components/jobdetails/company/Company.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Text, Image } from 'react-native'
+
+import styles from './company.style'
+import { checkImageURL } from '../../../utils'
+import { icons } from '../../../constants'
+
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image 
+          source={{ uri: checkImageURL(companyLogo)
+            ? companyLogo
+            : 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+
+      <View styles={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image 
+            source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage} 
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default Company
---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': '9a844fdfd3msh21bd2d5e797616dp1fb7dfjsn2e913a6a4dff',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        }
+        catch (error) {
+            setError(error);
+            alert('There is an error')
+        }   finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
